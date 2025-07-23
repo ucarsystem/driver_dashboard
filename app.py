bc288cde31771b82ea064a0547a7e270002727dc
ucarsystem <gyemsnet@gmail.com>
2025-07-23 13:15:17 +0900

    구성변경

--- app.py
+++ app.py
@@ -57,8 +57,8 @@ st.markdown("""
     /* 반응형 등급+달성율 */
     .grade-wrapper {
         width: 100px;
-        display: flex; 
         align-items: center; 
+        gap: 25px; 
         flex-wrap: nowrap;
     }
 
