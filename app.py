f6a1234d588076f9a3e99bd0b1e2404a1379414e
ucarsystem <gyemsnet@gmail.com>
2025-10-13 12:29:23 +0900

    구성변경

--- app.py
+++ app.py
@@ -451,9 +451,10 @@ if 조회버튼_클릭 :
                                 
                                 <div style="margin:15px;">
                                 <span style="font-size:17px;"><b>금월 나의 인센티브 (1개월 추정)</b></span><br>
-                                - 예상 기여액 : 2,800,000원<br>
-                                - 예상 배분액 : 280,000원<br>
-                                <span style="font-size:15px; color:gray;">(현재의 실적으로 1개월 추정)</span>
+                                - 예상 당월 배분액(해당 월 기준) : 20,000원<br>
+                                - 예상 총 배분액(이번 인센티브 기준) : 120.000원<br>
+                                <span style="font-size:15px; color:gray;">(현재의 실적으로 1개월 추정)</span><br>
+                                * 해당 금액은 예상 금액으로 실제와 다를 수 있음 *
                                 </div>
 
                                 <hr style="border: 0.5px solid #ccc;">
